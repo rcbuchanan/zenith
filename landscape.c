52a70f8a8657d9ed2368d123fcac29703b1d6316
digitalsigil <digitalsigil@gmail.com>
2014-12-15 20:45:57 -0600

    changes

--- landscape.c
+++ landscape.c
@@ -1,5 +1,6 @@
 #include <err.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_rng.h>
@@ -19,16 +20,25 @@
 #define ABS(x) (((x) > 0) ? (x) : (-(x)))
 
 
+struct hyperedge {
+	GLfloat	p[3];
+};
+
+struct hypernode {
+	GLuint	(*f)[3];
+	struct	 hyperedge *he[3];
+};
+
 struct landscape {
 	struct	GLvarray *vtx;
 	struct	GLbuffer *tri;
 };
 
 struct polyhedron {
-	GLfloat	*p;
-	int	 nps;
-	GLuint	*f;
-	int	 nfs;
+	GLfloat	(*p)[3];
+	int	 np;
+	GLuint	(*f)[3];
+	int	 nf;
 };
 
 
@@ -48,33 +58,33 @@ static	GLfloat ico_points [12][3] = {
 };
 
 static	GLuint ico_faces[20][3] = {
-	{0,  2,  1},
-	{0,  3,  2},
-	{0,  4,  3},
-	{0,  5,  4},
-	{0,  1,  5},
-	{1,  6, 10},
-	{1,  2,  6},
-	{2,  7,  6},
-	{2,  3,  7},
-	{3,  8,  7},
-	{3,  4,  8},
-	{4,  9,  8},
-	{4,  5,  9},
-	{5, 10,  9},
-	{5,  1, 10},
-	{10, 6, 11},
-	{6,  7, 11},
-	{7,  8, 11},
-	{8,  9, 11},
-	{9, 10, 11}
+	{ 1,  2,  0},
+	{ 2,  3,  0},
+	{ 3,  4,  0},
+	{ 4,  5,  0},
+	{ 5,  1,  0},
+	{10,  6,  1},
+	{ 6,  2,  1},
+	{ 6,  7,  2},
+	{ 7,  3,  2},
+	{ 7,  8,  3},
+	{ 8,  4,  3},
+	{ 8,  9,  4},
+	{ 9,  5,  4},
+	{ 9, 10,  5},
+	{10,  1,  5},
+	{11,  6, 10},
+	{11,  7,  6},
+	{11,  8,  7},
+	{11,  9,  8},
+	{11, 10,  9}
 };
 
 static	struct polyhedron ico_polyhedron = {
-	.p	= (GLfloat *) ico_points,
-	.nps	= 12,
-	.f	= (GLuint *) ico_faces,
-	.nfs	= 20,
+	.p	= ico_points,
+	.np	= 12,
+	.f	= ico_faces,
+	.nf	= 20,
 };
 	
 
@@ -120,8 +130,8 @@ landscape_create()
 	struct	landscape *l;
 	struct	polyhedron *p;
 	int	 i;
-	GLfloat	*fp;
-	GLuint	*up;
+	GLfloat	(*fp)[6];
+	GLuint	(*up)[3];
 
 	if (line_prog == NULL || shade_prog == NULL)
 		create_programs();
@@ -132,15 +142,14 @@ landscape_create()
 	if ((p = polyhedron_createsubdivided(&ico_polyhedron)) == NULL)
 		errx(1, __FILE__ ": failed to create polyhedron");
 
-	l->vtx = create_GLvarray(sizeof (GLfloat), 6 * p->nps);
-	fp = (GLfloat *) l->vtx->buf->d;
-	for (i = 0; i < p->nps; i++) {
-		fp[i * 6 + 0] = p->p[i * 3 + 0];
-		fp[i * 6 + 1] = p->p[i * 3 + 1];
-		fp[i * 6 + 2] = p->p[i * 3 + 2];
-		fp[i * 6 + 3] = p->p[i * 3 + 0];
-		fp[i * 6 + 4] = p->p[i * 3 + 1];
-		fp[i * 6 + 5] = p->p[i * 3 + 2];
+	l->vtx = create_GLvarray(sizeof (GLfloat), 6 * p->np);
+	for (fp = l->vtx->buf->d, i = 0; i < p->np; i++) {
+		fp[i][0] = p->p[i][0];
+		fp[i][1] = p->p[i][1];
+		fp[i][2] = p->p[i][2];
+		fp[i][3] = p->p[i][0];
+		fp[i][4] = p->p[i][1];
+		fp[i][5] = p->p[i][2];
 	}
 	glBindBuffer(GL_ARRAY_BUFFER, l->vtx->buf->id);
 	glBufferData(
@@ -149,10 +158,12 @@ landscape_create()
 		l->vtx->buf->d,
 		GL_STATIC_DRAW);
 
-	l->tri = create_GLbuffer(sizeof (GLuint), 3 * p->nfs);
-	up = (GLuint *) l->tri->d;
-	for (i = 0; i < p->nfs * 3; i++)
-		up[i] = (GLfloat) p->f[i];
+	l->tri = create_GLbuffer(sizeof (GLuint), 3 * p->nf);
+	for (up = l->tri->d, i = 0; i < p->nf; i++) {
+		up[i][0] = (GLfloat) p->f[i][0];
+		up[i][1] = (GLfloat) p->f[i][1];
+		up[i][2] = (GLfloat) p->f[i][2];
+	}
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, l->tri->id);
 	glBufferData(
 		GL_ELEMENT_ARRAY_BUFFER,
@@ -189,8 +200,9 @@ landscape_draw(struct landscape *l)
 		(void *) (sizeof (GLfloat) * 3));
 	glEnableVertexAttribArray(1);
 
+	//glEnable(GL_CULL_FACE);
 	//glFrontFace(GL_CCW);
-	//glCullFace(GL_BACK);
+	glCullFace(GL_FRONT);
 
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
@@ -199,7 +211,10 @@ landscape_draw(struct landscape *l)
 	glUniformMatrix4fv(0, 1, GL_TRUE, (float *) projection_modelview_collapse());
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(3);
 	glDrawElements(GL_TRIANGLES, l->tri->n * 3 / 4 + 3 * 5, GL_UNSIGNED_INT, 0);
+
+	glCullFace(GL_BACK);
 
 	glUseProgram(shade_prog->id);
 	glUniformMatrix4fv(0, 1, GL_TRUE, (float *) projection_modelview_collapse());
@@ -212,34 +227,72 @@ landscape_draw(struct landscape *l)
 	glDisable(GL_POLYGON_OFFSET_FILL);
 }
 
+static void
+update_adj(struct hypernode *hna, struct hypernode *hnb, struct hyperedge **hep)
+{
+	GLuint	*fa;
+	GLuint	*fb;
+
+	fa = *hna.f;
+	fb = *hnb.f;
+}
+
 static struct polyhedron *
 polyhedron_createsubdivided(struct polyhedron *in)
 {
-	struct	 polyhedron *out;
-	GLuint	 i;
+	struct	polyhedron *out;
 
-	if ((out = malloc(sizeof (struct polyhedron))) == NULL)
+	struct	hyperedge *he;
+	int	nhe;
+	struct	hypernode *hn;
+	int	nhn;
+	struct	hyperedge *hep;
+
+	GLuint	i;
+	GLuint	j;
+	
+	nhe	= in->nf * 3 / 2;
+	nhn	= in->nf;
+	
+	if ((he = malloc(sizeof (he[0]) * nhe)) == NULL)
+		errx(1, __FILE__ ": malloc");
+	if ((hn = malloc(sizeof (hn[0]) * nhn)) == NULL)
 		errx(1, __FILE__ ": malloc");
 
-	if ((out->p = malloc(sizeof (GLfloat) * in->nps * 3)) == NULL)
+	// nodes
+	memset(hn, 0, sizeof (hn[0]) * nhn);
+	for (i = 0; i < nhn; i++)
+		hn[i].f = in->f + i;
+
+	// edges
+	memset(he, 0, sizeof (he[0]) * nhe);
+	for (hep = he, i = 0; i < nhn; i++)
+		for (j = i + 1; j < nhn; j++)
+			update_adj(hn + i, hn + j, &hep);
+
+	if ((out = malloc(sizeof (out[0]))) == NULL)
 		errx(1, __FILE__ ": malloc");
-	if ((out->f = malloc(sizeof (GLuint) * in->nfs * 3)) == NULL)
+
+	out->np = nhn + nhe;
+	out->nf = nhn * 4;
+	if ((out->p = malloc(sizeof (out->p[0]) * out->np)) == NULL)
+		errx(1, __FILE__ ": malloc");
+	if ((out->f = malloc(sizeof (out->f[0]) * out->nf)) == NULL)
 		errx(1, __FILE__ ": malloc");
 
-	out->nps = in->nps;
-	out->nfs = in->nfs;
+	// copy original points verbatim
+	memcpy(out->p, in->p, sizeof (in->p[0]) * in->np);
 
-	for (i = 0; i < in->nps; i++) {
-		out->p[i * 3 + 0] = in->p[i * 3 + 0];
-		out->p[i * 3 + 1] = in->p[i * 3 + 1];
-		out->p[i * 3 + 2] = in->p[i * 3 + 2];
-	}
+	// copy subdivided points
+	for (i = 0; i < nhe; i++)
+		memcpy(out->p[nhn + i], he[i].p, sizeof (he[0].p));
 
-	for (i = 0; i < in->nfs; i++) {
-		out->f[i * 3 + 0] = in->f[i * 3 + 0];
-		out->f[i * 3 + 1] = in->f[i * 3 + 1];
-		out->f[i * 3 + 2] = in->f[i * 3 + 2];
-	}
+	// create new faces
+	for (i = 0; i < out->nf; i++)
+		memset(out->f[i], 0, sizeof (out->f[0]));
+
+	free (he);
+	free (hn);
 
 	return out;
 }
